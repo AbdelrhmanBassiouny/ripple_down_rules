847d1e17d61fefdebe761e6186ad8af584961ab5
A_Bassiouny <abdelrhman_bassuny@hotmail.com>
2025-02-03 11:51:11 +0100

    [RDR] doc.

--- src/pyrdr/rdr.py
+++ src/pyrdr/rdr.py
@@ -16,28 +16,53 @@ from .utils import tree_to_graph
 
 
 class Rule(NodeMixin):
-    conclusion: Category
     fired: bool = False
-    corner_case: Optional[Case] = None
+    """
+    Whether the rule has fired or not.
+    """
     refinement: Optional[Rule] = None
+    """
+    The refinement rule (called when this rule fires) of this rule if it exists.
+    """
     alternative: Optional[Rule] = None
+    """
+    The alternative rule (called when this rule doesn't fire) of this rule if it exists.
+    """
     all_rules: Dict[str, Rule] = {}
+    """
+    All rules in the classifier.
+    """
     rule_idx: int = 0
+    """
+    The index of the rule in the all rules list.
+    """
 
-    def __init__(self, conditions: Dict[str, Condition], category: Category,
+    def __init__(self, conditions: Dict[str, Condition], conclusion: Category,
                  edge_weight: Optional[str] = None,
                  parent: Optional[Rule] = None,
                  corner_case: Optional[Case] = None):
+        """
+        A rule in the ripple down rules classifier.
+
+        :param conditions: The conditions of the rule.
+        :param conclusion: The conclusion of the rule when the conditions are met.
+        :param edge_weight: The weight of the edge to the parent rule if it exists.
+        :param parent: The parent rule of this rule.
+        :param corner_case: The corner case that this rule is based on/created from.
+        """
         super(Rule, self).__init__()
         self.conditions = conditions
         self.corner_case = corner_case
-        self.conclusion = category
+        self.conclusion = conclusion
         self.parent = parent
         self.weight = edge_weight if parent is not None else None
         self._name = self.__str__()
         self.update_all_rules()
 
     def update_all_rules(self):
+        """
+        Update the all rules dictionary with this rule. And make the rule name unique if it already exists.
+        """
         if self.name in self.all_rules:
             self.all_rules[self.name].append(self)
             self.rule_idx = len(self.all_rules[self.name]) - 1
@@ -47,24 +72,25 @@ class Rule(NodeMixin):
 
     def _post_detach(self, parent):
         """
-        Called after this node is detached from the tree.
+        Called after this node is detached from the tree, useful when drawing the tree.
 
         :param parent: The parent node from which this node was detached.
         """
         self.weight = None
 
     def __call__(self, x: Case) -> Self:
-        return self.match(x)
+        return self.evaluate(x)
 
     def __getitem__(self, attribute_name):
         return self.conditions.get(attribute_name, None)
 
-    def match(self, x: Case) -> Rule:
+    def evaluate(self, x: Case) -> Rule:
         """
-        Check if the rule or its refinement or its alternative matches the case.
+        Check if the rule or its refinement or its alternative match the case,
+        by checking if the conditions are met, then return the rule that matches the case.
 
-        :param x: The case to match.
-        :return: The rule that matches the case.
+        :param x: The case to evaluate the rule on.
+        :return: The rule that fired or the last evaluated rule if no rule fired.
         """
         for att_name, condition in self.conditions.items():
             if att_name not in x.attributes or not condition(x.attributes[att_name].value):
@@ -74,37 +100,75 @@ class Rule(NodeMixin):
         refined_rule = self.refinement(x) if self.refinement else None
         return refined_rule if refined_rule and refined_rule.fired else self
 
-    def add_alternative(self, x: Case, conditions: Dict[str, Condition], category: Category):
+    def add_alternative(self, x: Case, conditions: Dict[str, Condition], conclusion: Category):
+        """
+        Add an alternative rule to this rule (called when this rule doesn't fire).
+
+        :param x: The case to add the alternative rule for.
+        :param conditions: The conditions of the alternative rule.
+        :param conclusion: The conclusion of the alternative rule.
+        """
         if self.alternative:
-            self.alternative.add_alternative(x, conditions, category)
+            self.alternative.add_alternative(x, conditions, conclusion)
         else:
-            self.alternative = self.add_connected_rule(conditions, category, x, edge_weight="else if")
+            self.alternative = self.add_connected_rule(conditions, conclusion, x, edge_weight="else if")
 
-    def add_refinement(self, x: Case, conditions: Dict[str, Condition], category: Category):
+    def add_refinement(self, x: Case, conditions: Dict[str, Condition], conclusion: Category):
+        """
+        Add a refinement rule to this rule (called when this rule fires).
+
+        :param x: The case to add the refinement rule for.
+        :param conditions: The conditions of the refinement rule.
+        :param conclusion: The conclusion of the refinement rule.
+        """
         if self.refinement:
-            self.refinement.add_alternative(x, conditions, category)
+            self.refinement.add_alternative(x, conditions, conclusion)
         else:
-            self.refinement = self.add_connected_rule(conditions, category, x, edge_weight="except if")
+            self.refinement = self.add_connected_rule(conditions, conclusion, x, edge_weight="except if")
 
-    def add_connected_rule(self, conditions: Dict[str, Condition], category: Category,
+    def add_connected_rule(self, conditions: Dict[str, Condition], conclusion: Category,
                            corner_case: Case,
-                           edge_weight: Optional[str] = None):
-        return Rule(conditions, category, corner_case=Case(corner_case.id_, list(corner_case.attributes.values())),
+                           edge_weight: Optional[str] = None) -> Rule:
+        """
+        Add a connected rule to this rule, connected in the sense that it has this rule as parent.
+
+        :param conditions: The conditions of the connected rule.
+        :param conclusion: The conclusion of the connected rule.
+        :param corner_case: The corner case of the connected rule.
+        :param edge_weight: The weight of the edge to the parent rule.
+        :return: The connected rule.
+        """
+        return Rule(conditions, conclusion, corner_case=Case(corner_case.id_, list(corner_case.attributes.values())),
                     parent=self, edge_weight=edge_weight)
 
     def get_different_attributes(self, x: Case) -> Dict[str, Attribute]:
+        """
+        Get the differentiating attributes between the corner case and the case.
+
+        :param x: The case to compare with the corner case.
+        :return: The differentiating attributes between the corner case and the case as a dictionary.
+        """
         return {a.name: a for a in self.corner_case.attributes.values()
                 if a not in x.attributes.values()}
 
     @property
     def name(self):
+        """
+        Get the name of the rule, which is the conditions and the conclusion.
+        """
         return self._name
 
     @name.setter
     def name(self, value):
+        """
+        Set the name of the rule.
+        """
         self._name = value
 
     def __str__(self, sep="\n"):
+        """
+        Get the string representation of the rule, which is the conditions and the conclusion.
+        """
         conditions = f"^{sep}".join([str(c) for c in list(self.conditions.values())])
         conditions += f"{sep}=> {self.conclusion.name}"
         return conditions
@@ -114,6 +178,9 @@ class Rule(NodeMixin):
 
 
 class RippleDownRules(ABC):
+    """
+    The abstract base class for the ripple down rules classifiers.
+    """
     start_rule: Optional[Rule] = None
     """
     The starting rule for the classifier tree.
@@ -123,6 +190,9 @@ class RippleDownRules(ABC):
     The figure to draw the tree on.
     """
     all_expert_answers: Optional[List[Dict[str, Condition]]] = None
+    """
+    All expert answers given by the expert function.
+    """
 
     @abstractmethod
     def classify(self, x: Case, target: Optional[Category] = None,
@@ -205,20 +275,22 @@ class RippleDownRules(ABC):
             de.to_picture(f"{filename}{'.png'}")
 
     def draw_tree(self):
-        """Draw the tree using matplotlib and networkx."""
+        """
+        Draw the tree using matplotlib and networkx.
+        """
         if self.start_rule is None:
             return
         self.fig.clf()
         graph = tree_to_graph(self.start_rule)
-        fig_sz_x = 10
-        fig_sz_y = 9
+        fig_sz_x = 13
+        fig_sz_y = 10
         self.fig.set_size_inches(fig_sz_x, fig_sz_y)
         pos = nx.drawing.nx_agraph.graphviz_layout(graph, prog="dot")
         # scale down pos
         max_pos_x = max([v[0] for v in pos.values()])
         max_pos_y = max([v[1] for v in pos.values()])
         pos = {k: (v[0] * fig_sz_x / max_pos_x, v[1] * fig_sz_y / max_pos_y) for k, v in pos.items()}
-        nx.draw(graph, pos, with_labels=True, node_color="lightblue", edge_color="gray", node_size=2000,
+        nx.draw(graph, pos, with_labels=True, node_color="lightblue", edge_color="gray", node_size=1000,
                 ax=self.fig.gca(), node_shape="o", font_size=8)
         nx.draw_networkx_edge_labels(graph, pos, edge_labels=nx.get_edge_attributes(graph, 'weight'),
                                      ax=self.fig.gca(), rotate=False, clip_on=False)
@@ -266,11 +338,24 @@ class SingleClassRDR(RippleDownRules):
 
 
 class MultiClassRDR(RippleDownRules):
+    """
+    A multi class ripple down rules classifier, which can draw multiple conclusions for a case.
+    This is done by going through all rules and checking if they fire or not, and adding stopping rules if needed,
+    when wrong conclusions are made to stop these rules from firing again for similar cases.
+    """
     def __init__(self, start_rules: Optional[List[Rule]] = None):
+        """
+        :param start_rules: The starting rules for the classifier, these are the rules that are at the top of the tree
+        and are always checked, in contrast to the refinement and alternative rules which are only checked if the
+        starting rules fire or not.
+        """
         self.start_rules = start_rules
 
     @property
     def start_rule(self):
+        """
+        Get the starting rule of the classifier.
+        """
         return self.start_rules[0] if self.start_rules else None
 
     def classify(self, x: Case, target: Optional[Category] = None,
@@ -325,6 +410,13 @@ class MultiClassRDR(RippleDownRules):
             rule_idx += 1
         return list(OrderedSet(conclusions))
 
-    def add_top_rule(self, conditions: Dict[str, Condition], category: Category, corner_case: Case):
-        self.start_rules.append(self.start_rules[-1].add_connected_rule(conditions, category, corner_case,
+    def add_top_rule(self, conditions: Dict[str, Condition], conclusion: Category, corner_case: Case):
+        """
+        Add a top rule to the classifier, which is a rule that is always checked and is part of the start_rules list.
+
+        :param conditions: The conditions of the rule.
+        :param conclusion: The conclusion of the rule.
+        :param corner_case: The corner case of the rule.
+        """
+        self.start_rules.append(self.start_rules[-1].add_connected_rule(conditions, conclusion, corner_case,
                                                                         edge_weight="next"))
