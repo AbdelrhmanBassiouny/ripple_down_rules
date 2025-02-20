a9493b67a6776f474d8091bd3980d58bfd2bba0e
A_Bassiouny <abdelrhman_bassuny@hotmail.com>
2025-02-20 23:29:38 +0100

    [RelationalRDR] small scrdr.

---
+++ src/ripple_down_rules/relational_datastructures.py
@@ -0,0 +1,6 @@
+"""
+These are Object Relational Mapping (ORM) classes that are used to map the relational data structures
+ to the object-oriented data structures, and vice versa.
+
+It mimics the classes in datastructures.py, but creates a relational equivalent to them.
+"""
