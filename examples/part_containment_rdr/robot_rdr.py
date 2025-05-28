662b33e211f7d9823b7716d74266c756c61d9049
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2025-05-28 14:51:37 +0200

    [RDRViews] first commit

--- examples/part_containment_rdr/robot_rdr.py
+++ examples/part_containment_rdr/robot_rdr.py
@@ -1,5 +1,6 @@
 from typing_extensions import Dict, Any
-from ripple_down_rules.rdr import GeneralRDR
+
+from ripple_down_rules.helpers import general_rdr_classify
 from ripple_down_rules.datastructures.case import Case, create_case
 from __main__ import Robot
 from . import robot_contained_objects_mcrdr as contained_objects_classifier
@@ -12,4 +13,4 @@ classifiers_dict['contained_objects'] = contained_objects_classifier
 def classify(case: Robot) -> Dict[str, Any]:
     if not isinstance(case, Case):
         case = create_case(case, max_recursion_idx=3)
-    return GeneralRDR._classify(classifiers_dict, case)
+    return general_rdr_classify(classifiers_dict, case)
