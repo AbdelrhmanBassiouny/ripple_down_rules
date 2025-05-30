dec769ca145ea24d3a6ea9e044df749b5aed6dcd
AbdelrhmanBassiouny <abassiou@uni-bremen.de>
2025-05-30 12:27:10 +0200

    [RDRCornerCase] use importlib reload to accomodate for changed files.

--- test/test_results/datasets_physical_object_select_objects_that_are_parts_of_robot/physical_object_select_objects_that_are_parts_of_robot_output__mcrdr_defs.py
+++ test/test_results/datasets_physical_object_select_objects_that_are_parts_of_robot/physical_object_select_objects_that_are_parts_of_robot_output__mcrdr_defs.py
@@ -1,8 +1,8 @@
-from datasets import PhysicalObject
-from typing import Dict
-from typing import List
-from typing import Union
 from datasets import Robot
+from typing import List
+from typing import Dict
+from datasets import PhysicalObject
+from typing import Union
 from typing_extensions import Union
 
 
