3d520a183db9646839445306e2bd23a06ab0084e
Abdelrhman Bassiouny <abdelrhman_bassuny@hotmail.com>
2025-07-16 15:21:25 +0200

    [RDR] need to fix rule not inheriting from new TrackedObjectMixin.

--- src/ripple_down_rules_meta/overrides.py
+++ src/ripple_down_rules_meta/overrides.py
@@ -8,7 +8,9 @@ from typing import ClassVar
 
 from typing_extensions import Type
 
-from ripple_down_rules import TrackedObjectMixin as OGTrackedObjectMixin, RDRDecorator
+from ripple_down_rules import TrackedObjectMixin as OGTrackedObjectMixin
+from ripple_down_rules.rdr_decorators import RDRDecorator
+from ripple_down_rules.rules import Rule
 
 _overrides = []
 
@@ -33,3 +35,18 @@ class TrackedObjectMixin(OGTrackedObjectMixin):
     @depends_on_rdr.decorator
     def depends_on(cls, tracked_object_type: Type[TrackedObjectMixin]) -> bool:
         ...
+
+    def __subclasshook__(cls, __subclass):
+        if cls is TrackedObjectMixin:
+            return True
+        return super().__subclasshook__(__subclass)
+
+    @classmethod
+    def __subclasses__(cls):
+        return super().__subclasses__() + [Rule]
+
+
+Rule.depends_on = TrackedObjectMixin.depends_on
+Rule.has = TrackedObjectMixin.has
+assert Rule._dependency_graph is TrackedObjectMixin._dependency_graph
+Rule.__subclasshook__ = TrackedObjectMixin.__subclasshook__
